a1b8f417a530bd14feb12d3c5532f27c269ca2f5
~traxh <127003785+traxhcxn@users.noreply.github.com>
2023-03-12 22:29:50 +0530

    Update SettersNGetters.py

--- SettersNGetters.py
+++ SettersNGetters.py
@@ -1,26 +1,20 @@
 #setters and getters using decorators
 class Student:
 	def __init__(self,name,roll_no,age):
-		self.__name=name
+		self.name=name
 		self.roll_no=roll_no
 		self.__age = age
 		
 	@property
 	def age(self):
 		return self.__age
-	def name(self):
-		return self.__name
-	
 	
 	@age.setter
 	def age(self, value):
 		self.__age = value
-	def name(self,value):
-		self.__name = value
-
-student1= Student('','22S11AAAA','')
-student1.age = 19
-student1.name='xxx'
+	
+student1= Student("student's_name","22S11AAAA","")
+student1.age = 18
 print(student1.name)
 print(student1.roll_no)
 print(student1.age)
