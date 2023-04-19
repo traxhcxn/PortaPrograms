3f9a084c00741c1987b385d0fce0c338bcee17a9
~traxh <127003785+traxhcxn@users.noreply.github.com>
2023-04-19 22:37:38 +0530

    Update stack_ll.py

--- stack_ll.py
+++ stack_ll.py
@@ -1,67 +1,55 @@
 #stack using linked list
-
-class node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-
-class stack:
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Stack:
     def __init__(self):
-        self.head =None
-
+        self.head = None
     def is_empty(self):
-        if self.head==None:
+        if self.head == None:
             return True
         else:
             return False
-
-    def push(self,data):
-        if self.head==None:
-            self.head=node(data)
+    def push(self, data):
+        if self.head == None:
+            self.head = Node(data)
         else:
-            new_node=node(data)
-            new_node.next=self.head
-            self.head=new_node
-
+            newnode = Node(data)
+            newnode.next = self.head
+            self.head = newnode
     def pop(self):
         if self.is_empty():
             return None
         else:
-            popped_node=self.head
-            self.head=self.head.next
-            popped_node.next=None
+            popped_node = self.head
+            self.head = self.head.next
+            popped_node.next = None
             return popped_node.data
-
     def peek(self):
         if self.is_empty():
             return None
         else:
             return self.head.data
-
-
     def display(self):
-        iternode=self.head
-        if self.is_empty:
-            print("Stack underflow.")
+        iternode = self.head
+        if self.is_empty():
+            print("Stack Underflow")
         else:
-            while(iternode!=None):
-                print(iternode.data,"-->",end="")
-                iternode=iternode.next
-                if (iternode!=None):
-                    print("-->",end="")
-            return
-
-new_stack=stack()
-new_stack.push(10)
-new_stack.push(12)
-new_stack.push(13)
-new_stack.push(14)
-
-new_stack.display()
-
-print("Top element is:",new_stack.peek())
-new_stack.pop()
-new_stack.pop()
-new_stack.display()
-
-print("Topn element is:",new_stack.peek())
+            while(iternode != None):
+                print(iternode.data, end = "")
+                iternode = iternode.next
+                if(iternode != None):
+                    print(" -> ", end = "")
+        return
+stack = Stack()
+stack.push(10)
+stack.push(12)
+stack.push(13)
+stack.push(14)
+stack.display()
+print("\nTop element is ", stack.peek())
+stack.pop()
+stack.pop()
+stack.display()
+print("\nTop element is ", stack.peek())
