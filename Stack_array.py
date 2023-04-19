569a0bbe76ab4a4e3802696c5329edaede181aaa
~traxh <127003785+traxhcxn@users.noreply.github.com>
2023-04-19 22:07:17 +0530

    Add files via upload

---
+++ Stack_array.py
@@ -0,0 +1,18 @@
+#stacks using array
+
+stack=[]
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print("Initial Stack:")
+print(stack)
+
+print("Elements popped from the list are:")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print("Stack after elements are popped:")
+print(stack)
