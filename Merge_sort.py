c7f3e60320be0cf55d35354399739bece70b5be0
~traxh <127003785+traxhcxn@users.noreply.github.com>
2023-04-19 22:05:13 +0530

    Update Merge_sort.py

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,21 +1,16 @@
 # a python program to implement merge sort algorithm.
 def merge(arr, l, m, r):
     n1 = m - l + 1
-    n2 = r - m
-
-	
+    n2 = r - m	
     L = [0] * (n1)
     R = [0] * (n2)
-
     for i in range(0, n1):
         L[i] = arr[l + i]
     for j in range(0, n2):
         R[j] = arr[m + 1 + j]
-
     i = 0	 
     j = 0	
     k = l	
-
     while i < n1 and j < n2:
         if L[i] <= R[j]:
             arr[k] = L[i]
@@ -23,8 +18,7 @@ def merge(arr, l, m, r):
         else:
              arr[k] = R[j]
              j += 1
-        k += 1
-		
+        k += 1		
     while i < n1:
         arr[k] = L[i]
         i += 1
@@ -33,7 +27,6 @@ def merge(arr, l, m, r):
         arr[k] = R[j]
         j += 1
         k += 1
-
 def mergeSort(arr, l, r):
     if l < r:
         m = l+(r-l)//2
@@ -41,7 +34,6 @@ def mergeSort(arr, l, r):
         mergeSort(arr, l, m)
         mergeSort(arr, m+1, r)
         merge(arr, l, m, r)
-
 arr = [12, 11, 13, 5, 6, 7]
 n = len(arr)
 print("Given array is\n",arr)
